fdd9b00c78268455d79c120f0c866a2eaa0b029c
ohzecodes <mo.ze@outlook.com>
2024-06-05 20:10:43 +0500

    fix the title

--- src/main/java/main/chequewriter/MainApplication.java
+++ src/main/java/main/chequewriter/MainApplication.java
@@ -13,7 +13,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 989);
-        stage.setTitle("Hello!");
+        stage.setTitle("Cheque Writter!");
         stage.setScene(scene);
         stage.show();
     }
